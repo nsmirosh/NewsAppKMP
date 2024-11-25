df6cf520726b30c599b5323b2c61e6b4f40aeab1
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2024-11-25 19:56:59 +0700

    Finished with the details screen

--- composeApp/src/commonMain/kotlin/nick/mirosh/newsappkmp/ui/article/DetailsScreenContent.kt
+++ composeApp/src/commonMain/kotlin/nick/mirosh/newsappkmp/ui/article/DetailsScreenContent.kt
@@ -5,17 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import coil3.compose.AsyncImage
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format.DateTimeComponents
 import nick.mirosh.newsapp.domain.feed.model.Article
 import org.jetbrains.compose.resources.painterResource
@@ -37,15 +39,26 @@ fun DetailsScreenContent(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
     ) {
-        Text(article.title)
-        Text(article.author)
-        Text(formatDateTime(article.publishedAt))
-        val imageModifier = Modifier.fillMaxWidth()
+        Text(
+            text = article.title,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = article.author,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Text(
+            fontSize = 12.sp,
+            color = Color.Gray,
+            text = formatDateTime(article.publishedAt),
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        val imageModifier = Modifier.fillMaxWidth().padding(top = 16.dp)
         if (!LocalInspectionMode.current) {
             AsyncImage(
-                contentScale = ContentScale.Crop,
+                contentScale = ContentScale.Fit,
                 modifier = imageModifier,
                 model = article.urlToImage,
                 contentDescription = "Article image"
@@ -59,7 +72,7 @@ fun DetailsScreenContent(
             )
         }
 
-        Text(article.content)
+        Text(text = article.content, modifier = Modifier.padding(16.dp))
     }
 }
 
